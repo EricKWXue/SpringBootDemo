b60811e5e84f68b4fce85e4e366d876de7a6a6bf
EricKWXue <xuekw1101@gmail.com>
2020-02-08 23:01:41 +0800

    add thymeleaf demo

---
+++ src/main/java/com/eric/springdemo/controller/ThymeleafController.java
@@ -0,0 +1,17 @@
+package com.eric.springdemo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/thymeleaf")
+public class ThymeleafController {
+
+    @GetMapping(value = "/welcome")
+    public String toIndex(Model model){
+        model.addAttribute("name", "Eric");
+        return "welcome";
+    }
+}
