c12d56534418fa3c2af234fb0f976233fde61be4
Eric_Xue <Eric_Xue@asus.com>
2020-07-16 18:16:18 +0800

    update threadpool

--- src/main/java/com/eric/springdemo/config/ThreadPoolConfig.java
+++ src/main/java/com/eric/springdemo/config/ThreadPoolConfig.java
@@ -12,18 +12,21 @@ import java.util.concurrent.ThreadPoolExecutor;
 @EnableAsync
 public class ThreadPoolConfig {
 
-    @Bean
+    @Bean("threadPoolExecutor")
     public Executor threadPoolExecutor(){
         ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        //此方法返回可用处理器的虚拟机的最大数量; 不小于1
+        int coreSize = Runtime.getRuntime().availableProcessors();
         //配置核心线程数
-        executor.setCorePoolSize(20);
+        executor.setCorePoolSize(coreSize);
         //配置最大线程数
-        executor.setMaxPoolSize(200);
+        executor.setMaxPoolSize(coreSize*2 + 1);
+        //除核心线程外的线程存活时间
+        executor.setKeepAliveSeconds(3);
         //配置队列大小
         executor.setQueueCapacity(2147483647);
         //配置线程池中的线程的名称前缀
         executor.setThreadNamePrefix("async-service-");
-
         // rejection-policy：当pool已经达到max size的时候，如何处理新任务
         // CALLER_RUNS：不在新线程中执行任务，而是有调用者所在的线程来执行
         executor.setRejectedExecutionHandler(new ThreadPoolExecutor.CallerRunsPolicy());
